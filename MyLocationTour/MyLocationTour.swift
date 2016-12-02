7e53f0a51703bd471fd2d785174fe7bc43954b60
Joao Ramos <joaoramos@Joaos-Mac.local>
2016-12-02 12:31:05 +0000

    first commit

---
+++ MyLocationTour/MyLocationTour.swift
@@ -0,0 +1,13 @@
+//
+//  MyLocationTour.swift
+//  MyLocationTour
+//
+//  Created by Joao Ramos on 01/12/2016.
+//  Copyright © 2016 Luis Torres. All rights reserved.
+//
+
+import UIKit
+
+class MyLocationTour: NSObject {
+
+}
---
+++ MyLocationTourPin.swift
@@ -0,0 +1,13 @@
+//
+//  MyLocationTourPin.swift
+//  MyLocationTour
+//
+//  Created by Joao Ramos on 01/12/2016.
+//  Copyright © 2016 Luis Torres. All rights reserved.
+//
+
+import UIKit
+
+class MyLocationTourPin: NSObject {
+
+}
